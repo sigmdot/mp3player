4dff03e1cbec7b553ff5df4728b6f3c3b392e878
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-07-22 18:16:01 -0400

    creado boton collapse

--- src/components/Listsong.vue
+++ src/components/Listsong.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="col-12">
+  <div class="col-12 p-0">
+      <button type="button" v-b-toggle.collapse-1 class="btn btn-outline-secondary w-100 row m-0">
+          <div class="col-10 p-0 text-center">
+              Mi playlist!
+          </div>
+          <div class="col-2">
 
+          </div>
+      </button>
   </div>
 </template>
 
