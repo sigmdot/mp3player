34e0d7c41733872723deaaf16ba10b0e330c9b24
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-07-22 17:12:02 -0400

    creado analyzerAudio component

---
+++ src/components/Audioanalyzer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="mp3">
+    <audio src="../assets/music/1.mp3" id="audio" controls class="col-12 p-0 w-100"></audio>
+  </div>
+</template>
+
+<script>
+
+import AudioMotionAnalyzer from "audiomotion-analyzer";
+
+export default {
+    name:'AudioAnalyzer',
+    mounted(){
+        const sizelol = document.getElementById('mp3').style.width;
+        console.log(sizelol);
+        const audioMotion = new AudioMotionAnalyzer(
+      document.getElementById("mp3"),
+      {
+        source: document.getElementById("audio"),
+        gradient:'classic',
+        showLeds:true,
+        smoothing:0.9,
+        mode:6,
+        height:0,
+        width:0
+
+      })
+      console.log(audioMotion)
+    }
+}
+</script>
+
+<style>
+canvas{
+  width: 100% !important;
+  height: 300px !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="home">
-     
+  <div class="home container-fluid border">
+     <div class="row m-0">
+       <div class="col-lg-8 col-12 row border m-0">
+         <div class="col-12 p-0">
+           <AudioAnalyzer></AudioAnalyzer>
+         </div>
+         <div class="col-12" id="formulario">
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae architecto, voluptates porro soluta, ex sed voluptate harum commodi rem id quas dolores fugit sit odio tempora, possimus sapiente doloremque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolores quas, cum illo minima, molestiae deserunt facere accusantium eveniet vel exercitationem quo facilis iure assumenda sunt odit porro perspiciatis beatae?</p>
+         </div>
+       </div>
+       <div class="col-lg-4 col-12 row border m-0">
+         <div class="col-12">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, veritatis. Provident sunt blanditiis accusantium magnam quisquam ex perspiciatis, quos vitae non! Placeat ut mollitia voluptatibus. Iusto nobis dolorum iste facilis! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste numquam accusamus quo vitae, tempora incidunt. Ullam mollitia quos quaerat eligendi ex perferendis repellendus quia? Magni necessitatibus delectus aspernatur quia nisi!</p>
+         </div>
+         <div class="col-12">
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates repellendus enim provident itaque quia perspiciatis, unde impedit. Veniam quae quos illum. Deleniti corrupti animi cupiditate, recusandae aspernatur numquam vel assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, in. Vel nostrum velit, exercitationem nihil quo doloremque, fuga ullam adipisci vitae, aliquid ea quas. Ratione sapiente eveniet aut itaque pariatur.</p>
+         </div>
+       </div>
+     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-/* import HelloWorld from '@/components/HelloWorld.vue' */
+import AudioAnalyzer from '@/components/Audioanalyzer.vue'
 
 export default {
   name: 'Home',
   components: {
-    
+    AudioAnalyzer
   }
 }
 </script>
